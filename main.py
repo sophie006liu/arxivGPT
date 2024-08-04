763bc6473a31028eaf2ff4d2c65c1b14cc129635
sophie006liu <41531009+sophie006liu@users.noreply.github.com>
2024-08-03 21:09:39 -0700

    clean up files and data

--- main.py
+++
@@ -1,8 +0,0 @@
-from get_all_from_subtopic import scrape_articles
-import pickle
-
-with open ('outfile', 'rb') as fp:
-    itemlist = pickle.load(fp)
-
-for link in itemlist:
-    scrape_articles(link)
